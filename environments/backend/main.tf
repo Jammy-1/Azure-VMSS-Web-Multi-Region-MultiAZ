b0bb7ef7b5b250b2af56edd5b06d1d7fd76ad1f2
Jammy-1 <jplested132@gmail.com>
2025-12-29 20:47:50 +0000

    - Backend Enviroment

--- environments/backend/main.tf
+++ environments/backend/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.storage_container_name
+    key                  = var.state_key
+  }
+}
+
 # Resource Group
 module "resource-group" {
   source              = "../../Modules/Resource-Group"
