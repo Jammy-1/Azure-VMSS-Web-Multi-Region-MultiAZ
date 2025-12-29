f85030912672ae09423894a638d1d01f37b10fb3
Jammy-1 <jplested132@gmail.com>
2025-12-29 20:48:26 +0000

    - Bootstrap Enviroment

---
+++ environments/backend/bootstrap/bootstrap.tf
@@ -0,0 +1,22 @@
+# Resource Group
+module "resource-group" {
+  source              = "../../../Modules/Resource-Group"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  tags = merge(var.env_tags, var.backend_tags)
+}
+
+# Storage
+module "storage" {
+  source                 = "../../../Modules/Storage"
+  resource_group_name    = var.resource_group_name
+  storage_account_name   = var.storage_account_name
+  storage_container_name = var.storage_container_name
+  state_key              = var.state_key
+  location               = var.location
+
+  depends_on = [module.resource-group]
+
+  tags = merge(var.env_tags, var.backend_tags)
+}
