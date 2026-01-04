338260ee6c85bb95a89f0039f4c257a5ef9641ec
Jammy-1 <jplested132@gmail.com>
2026-01-04 16:32:59 +0000

    FMT

--- Modules/Event-Hub/outputs.tf
+++ Modules/Event-Hub/outputs.tf
@@ -2,4 +2,4 @@
 output "eventhub_namespace_name" { value = azurerm_eventhub_namespace.this.name }
 output "eventhub_name" { value = azurerm_eventhub.this.name }
 output "eventhub_auth_rule_name" { value = azurerm_eventhub_namespace_authorization_rule.this.name }
-output "eventhub_auth_rule_id" { value =  azurerm_eventhub_namespace_authorization_rule.this.id }
+output "eventhub_auth_rule_id" { value = azurerm_eventhub_namespace_authorization_rule.this.id }
