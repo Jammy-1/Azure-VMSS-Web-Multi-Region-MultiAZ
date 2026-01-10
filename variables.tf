a0c438fd64fa25741241e713039657e8c4dc9a9e
Jammy-1 <jplested132@gmail.com>
2026-01-10 21:57:01 +0000

    - Terraform Root; Removed Backend Variables

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-# Backend 
-variable "storage_account_name" { type = string }
-variable "storage_container_name" { type = string }
-variable "state_key" { type = string }
-
 # General 
 variable "resource_group_name" { type = string }
 variable "location" { type = string }
